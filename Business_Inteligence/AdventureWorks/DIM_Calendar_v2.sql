ea212c22432e8aed67ca95170b8bb2957b951972
Bruno Reis <bru401@gmail.com>
2025-09-19 19:05:25 -0300

    Add files via upload

---
+++ Business_Inteligence/AdventureWorks/DIM_Calendar_v2.sql
@@ -0,0 +1,25 @@
+-- Limpando a tabela DIMDate
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date, 
+  --,[DayNumberOfWeek], 
+  [EnglishDayNameOfWeek] AS Day, 
+  --,[SpanishDayNameOfWeek]
+  --,[FrenchDayNameOfWeek]
+  --,[DayNumberOfMonth] 
+  --, [DayNumberOfYear  
+  [WeekNumberOfYear] AS WeekNum, 
+  [EnglishMonthName] AS Month,
+  LEFT([EnglishMonthName], 3) AS MonthShort,
+  --,[SpanishMonthName]
+  --,[FrenchMonthName]
+  [MonthNumberOfYear] AS MonthNum, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year 
+  --,[CalendarSemester]
+  --,[FiscalQuarter]
+  --,[FiscalYear]
+  --,[FiscalSemester]
+FROM 
+  [AdventureWorksDW2022].[dbo].[DimDate]
+
